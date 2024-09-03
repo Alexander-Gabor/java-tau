52718cc75a2e3b6ed8b864aaa70cf0d164f378c8
Alexander-Gabor <gaboralexanderalex@gmail.com>
2024-09-03 20:15:05 +0200

    DataTypes converted tested

---
+++ src/Chapter8/DataTypes.java
@@ -0,0 +1,16 @@
+package Chapter8;
+
+public class DataTypes {
+
+    public static void main(String[] args){
+        convertDecimalToInt();
+    }
+
+    public static void convertDecimalToInt(){
+        double[] numbers = {1.5, 3.99, 45.5, 222.6};
+
+        for(Double number : numbers){
+            System.out.println(number.intValue());
+        }
+    }
+};
