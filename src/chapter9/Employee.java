d97557dc286c42d1e1744d06ee073cdc98f56db9
Alexander-Gabor <gaboralexanderalex@gmail.com>
2024-09-05 19:46:54 +0200

    InheritanceTester ran as expected

--- src/chapter9/Employee.java
+++ src/chapter9/Employee.java
@@ -1,5 +1,23 @@
 package chapter9;
 
-public class Employee {
+public class Employee extends Individual {
 
+    private String employeeId;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
 }
---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,12 @@
+package chapter9;
+
+public class InheritanceTester {
+
+    public static void main(String[] args){
+
+        Individual individual = new Individual();
+        Employee employee + new Employee():
+//        individual.
+//        employee.
+    }
+}
