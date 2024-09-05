e5733abae5ebe1556c54c20d793ee8024f8a78ae
Alexander-Gabor <gaboralexanderalex@gmail.com>
2024-09-05 19:34:23 +0200

    chapter9 classes created

--- src/chapter8/TextSpacer.java
+++ src/chapter8/TextSpacer.java
@@ -1,4 +1,21 @@
 package chapter8;
 
 public class TextSpacer {
+    public static void main (String [] args){
+        addSpaces("Hey!It'sAlexander");
+    }
+
+    public static void addSpaces(String text){
+
+        var modifiedText = new StringBuilder(text);
+
+        for(int i=0; i<modifiedText.length(); i++){
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+
+        System.out.println(modifiedText);
+    }
 }
